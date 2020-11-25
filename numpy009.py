4dd1073e12b618940f145b68a94a277dff282888
aktsan <aktbox6@yahoo.co.jp>
2020-11-25 11:23:59 +0900

    first add

---
+++ numpy009.py
@@ -0,0 +1,4 @@
+#Create a 3x3 matrix with values ranging from 0 to 8 (★☆☆)
+import numpy as np
+a = np.arange(9).reshape(3,3)
+print(a)
---
+++ numpy010.py
@@ -0,0 +1,5 @@
+#Find indices of non-zero elements from [1,2,0,0,4,0] (★☆☆)
+import numpy as np
+a = np.array([1,2,0,0,4,0])
+print(a)
+print(a.nonzero())
