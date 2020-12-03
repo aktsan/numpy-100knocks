4b9b10f074906e0f9ac703d01f2b5869c5a06673
aktsan <aktbox6@yahoo.co.jp>
2020-12-03 20:30:10 +0900

    add

---
+++ Lasthalf/numpy056.py
@@ -0,0 +1,7 @@
+#Generate a generic 2D Gaussian-like array (★★☆)
+import numpy as np
+X, Y = np.meshgrid(np.linspace(-1,1,10), np.linspace(-1,1,10))
+D = np.sqrt(X*X+Y*Y)
+sigma, mu = 1.0, 0.0
+G = np.exp(-( (D-mu)**2 / ( 2.0 * sigma**2 ) ) )
+print(G)
