72c12530fdac00f3b9ee697295d062c9e2c4d3c7
aktsan <aktbox6@yahoo.co.jp>
2020-12-14 23:11:39 +0900

    add

---
+++ Lasthalf/numpy064.py
@@ -0,0 +1,5 @@
+# Consider a given vector, how to add 1 to each element indexed by a second vector (be careful with repeated indices)? (★★★)
+import numpy as np
+A = np.ones(10)
+B = np.ones(10)
+print(A+B)
